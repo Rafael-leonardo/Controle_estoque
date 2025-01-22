5fc256a864958ad6f611ca52305bf263ee2d6b81
Rafael-leonardo <Rafaelleolopes123@gmail.com>
2025-01-22 02:43:13 -0300

    deploy teste

--- projeto/settings.py
+++ projeto/settings.py
@@ -12,7 +12,9 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 from decouple import config
+import os
 
+PORT = os.getenv('PORT', '8000')
 # Construção de caminhos dentro do projeto como: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
